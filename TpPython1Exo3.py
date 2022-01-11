d581cf17a14162d4231a1140d0ee758138ad5fe4
Maxime Torlois <maximetorlois@gmail.com>
2022-01-11 17:05:07 +0100

    Modification exo 3

--- TpPython1Exo3.py
+++ TpPython1Exo3.py
@@ -84,42 +84,49 @@ import re
 
 import csv
 from datetime import date
+continuer = True
+while continuer:
+    with open(f'{date.today().year}-{date.today().month}-{date.today().day}.csv', 'a', newline='') as csvfile:
 
-with open(f'{date.today().year}-{date.today().month}-{date.today().day}.csv', 'a', newline='') as csvfile:
+        nom = input("indiquez votre nom\n")
+        prenom = input("indiquez votre prenom\n")
+        year = input("indiquez votre année de naissance\n")
+        try:
+            year = int(year)
+        except ValueError:
+            #print("veuillez indiquer l'année en chiffres\n")
+            year = input("Veuillez indiquer l'année en chiffres\n")
 
-    nom = input("indiquez votre nom\n")
-    prenom = input("indiquez votre prenom\n")
-    year = input("indiquez votre année de naissance\n")
-    try:
-        year = int(year)
-    except ValueError:
-        #print("veuillez indiquer l'année en chiffres\n")
-        year = input("Veuillez indiquer l'année en chiffres\n")
+        longueur_year = len(str(year))
+        if len(str(year)) != 4:
+            year = input("Veuillez indiquer un chiffre de 4 caractères\n")
+
+        if year > 2016:
+            groupe = "n'est pas admis"
+        elif year < 2016 and year > 2010:
+            groupe = "est admis en Poussin"
+        elif year < 2010 and year > 2002:
+           groupe = "est admis en Cadet"
+        elif year < 2002 and year > 1998:
+            groupe = "est admis en Junior"
+        elif year < 1998 and year > 1992:
+            groupe = "est admis en Semi-pro"
+        elif year < 1992 and year == 1982:
+            groupe = "est admis en Pro"
+        elif year < 1982:
+            groupe = "est admis en admis"
+
+        mail = input("indiquez votre adresse mail\n")
+
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([f'{nom}'] + [f'{prenom}'] + [f'née en {year}'] + [f'{groupe}'])
+        spamwriter.writerow([f'{nom}', f'{prenom}', f'née en {year}', f'{groupe}'])
+
+    rep = input("avez vous d'autre personnes a inscrire ? y/n\n")
+    if rep == "n":
+        continuer = False
+        print("fini")
     else:
-       continuer = False
-
-    longueur_year = len(str(year))
-    if len(str(year)) != 4:
-        year = input("Veuillez indiquer un chiffre de 4 caractères\n")
-
-    if year > 2016:
-        groupe = "Vous n'etes pas admis"
-    elif year < 2016 and year > 2010:
-        groupe = "Vous etes Poussin"
-    elif year < 2010 and year > 2002:
-       groupe = "Vous etes Cadet"
-    elif year < 2002 and year > 1998:
-        groupe = "Vous etes admis en Junior"
-    elif year < 1998 and year > 1992:
-        groupe = "Vous etes admins en Semi-pro"
-    elif year < 1992 and year == 1982:
-        groupe = "Vous etes admis en Pro"
-    elif year < 1982:
-        groupe = "Vous n'etes pas admis"
-
-    mail = input("indiquez votre adresse mail\n")
-
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow([f'{nom}'] + [f'{prenom}'] + [f'née en{year}'] + [f'{groupe}'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+        continuer = True
+        
